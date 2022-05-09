9315407aca45425542cf8ab4b64573be28e6f68e
Stephenzcc <401186943jrf@gmail.com>
2022-05-09 16:06:08 +0800

    1

---
+++ src/component/Rangeset.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Rangeset() {
+  return <div>Rangeset</div>;
+}
+
+export default Rangeset;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "antd/dist/antd.min.css";
+import "./index.css";
+import App from "./App";
+import { Layout, Row, Col } from "antd";
+import { Content } from "antd/lib/layout/layout";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
+    <Layout>
+      <Content>
+        <Row>
+          <Col
+            span={10}
+            style={{
+              backgroundColor: "red",
+              height: "200px",
+            }}
+          ></Col>
+
+          <Col
+            span={10}
+            style={{
+              backgroundColor: "blue",
+              height: "200px",
+            }}
+          ></Col>
+        </Row>
+        <Row>
+          <Col
+            span={10}
+            style={{
+              backgroundColor: "yellow",
+              height: "200px",
+            }}
+          ></Col>
+          <Col
+            span={10}
+            style={{
+              backgroundColor: "green",
+              height: "200px",
+            }}
+          ></Col>
+        </Row>
+      </Content>
+    </Layout>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
