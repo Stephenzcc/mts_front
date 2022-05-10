32a65bda4295ed8ddc048e3608b6417b4f5dbd78
Stephenzcc <401186943jrf@gmail.com>
2022-05-10 20:35:36 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MTSLayout from "./component/MTSLayout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MTSLayout />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "antd/dist/antd.min.css";
 import "./index.css";
 import App from "./App";
-import { Layout, Row, Col } from "antd";
-import { Content } from "antd/lib/layout/layout";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,42 +10,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <Layout>
-      <Content>
-        <Row>
-          <Col
-            span={10}
-            style={{
-              backgroundColor: "red",
-              height: "200px",
-            }}
-          ></Col>
-
-          <Col
-            span={10}
-            style={{
-              backgroundColor: "blue",
-              height: "200px",
-            }}
-          ></Col>
-        </Row>
-        <Row>
-          <Col
-            span={10}
-            style={{
-              backgroundColor: "yellow",
-              height: "200px",
-            }}
-          ></Col>
-          <Col
-            span={10}
-            style={{
-              backgroundColor: "green",
-              height: "200px",
-            }}
-          ></Col>
-        </Row>
-      </Content>
-    </Layout>
   </React.StrictMode>
 );
